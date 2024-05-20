4078d446d9e11c17ca22d6271f67fa8950aace72
Aayush Tamang <aiyoustmg@gmail.com>
2024-05-20 16:04:55 +0545

    Updated

---
+++ src/RootLayout.jsx
@@ -0,0 +1,27 @@
+import PostsList from "./components/PostsList"
+import "./App.css"
+import MainHeader from "./components/MainHeader"
+import { useState } from "react"
+
+function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  function closeModalHandler() {
+    setIsModalVisible(false)
+  }
+  function openModalHandler() {
+    setIsModalVisible(true)
+  }
+  return (
+    <>
+      <MainHeader onCreatePost={openModalHandler}></MainHeader>
+      <main>
+        <PostsList
+          isPosting={isModalVisible}
+          onStopPosting={closeModalHandler}
+        ></PostsList>
+      </main>
+    </>
+  )
+}
+
+export default App
